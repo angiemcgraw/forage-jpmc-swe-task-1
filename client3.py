3f3545056c7edffedc07c357c62d819ed2f2fdd2
Angie McGraw <angiebmcgraw@gmail.com>
2023-07-26 10:24:20 -0400

    Updated getDataPoint(), getRatio(), and main

--- client3.py
+++ client3.py
@@ -31,18 +31,20 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    # The price of the stock is calculated as the average of the bid price and the ask price
+    price = (bid_price + ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    # Case: error when dividing by 0
+    if price_b == 0:
+        return
+    return price_a/price_b
 
 
 # Main
@@ -51,9 +53,12 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Initialize prices dictionary (key, value) = (stock, price)
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Get the ratio
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
--- client_test.py
+++ client_test.py
@@ -20,6 +20,5 @@ class ClientTest(unittest.TestCase):
   """ ------------ Add more unit tests ------------ """
 
 
-
 if __name__ == '__main__':
     unittest.main()
